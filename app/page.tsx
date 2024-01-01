e287c24ca66bddb038b37b98942617e5067b7ddf
julien <julien.milants@gmail.com>
2024-01-01 21:51:29 +0100

    add liste

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../src/scss/App.scss";
 import Navbar from "@/src/Components/Navbar";
 import Carousel from "@/src/Components/Widget/Carousel";
+import { Home_Text } from "@/src/ListText/Home_Text";
 export default function page() {
   return (
     <>
@@ -9,14 +10,19 @@ export default function page() {
       <div className="Home_Container">
         <div className="title">
           <h1>
-            Explorez les performances de vos joueurs préférés, quels que soient
-            les jeux, et concrétisez vos rêves avec l&#39;achat des
-            périphériques ultimes.
+            {Home_Text[0].explo}
           </h1>
         </div>
       </div>
       <div className="Section-Two">
-        <Carousel />
+        {/*<Carousel />*/}
+      </div>
+      <div className="Section-Three">
+        <div className="why">
+          <div className="title">
+            <h1>{Home_Text[1].why}</h1>
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ src/ListText/Home_Text.ts
@@ -0,0 +1,4 @@
+export const Home_Text = [
+    {explo: "Explorez les performances de vos joueurs préférés, quels que soient les jeux, et concrétisez vos rêves avec l&#39;achat des périphériques ultimes."},
+    {why: "Pourquoi Nous ?"}
+]
