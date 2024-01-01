6b6e7e08e53ce59fedc53c2cda87d0e2b2a9731d
julien <julien.milants@gmail.com>
2024-01-01 20:42:00 +0100

    add responsive for navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,25 +1,38 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
 import { List_Navbar } from "../ListText/List_Navbar";
 import search from "../img/icons/searc.svg";
 import Image from "next/image";
 import Link from "next/link";
+
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <>
-      <header className="Navbar_Container">
+      <header className={`Navbar_Container ${showMenu ? "open" : ""}`}>
         <nav className="content">
           <div className="title">
             <h1>{List_Navbar[0].title}</h1>
           </div>
-          <div className="items">
+          <div className="burgerButton" onClick={toggleMenu}>
+            ☰
+          </div>
+          
+          <div className={`items ${showMenu ? "open" : ""}`}>
             <Link href="/pages/players">{List_Navbar[1].items}</Link>
             <Link href="/pages/team">{List_Navbar[2].items}</Link>
             <Link href="/pages/events">{List_Navbar[3].items}</Link>
             <Link href="/pages/contact">{List_Navbar[4].items}</Link>
           </div>
-          <div className="icons">
+          <div className="icons" onClick={toggleMenu}>
             <Image src={search} alt="Search Icons" className="img" />
           </div>
+          
         </nav>
       </header>
     </>
