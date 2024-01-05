76bd83071dda7fbd8d86e598602e39e6b0eda6a2
julien <julien.milants@gmail.com>
2024-01-05 11:33:34 +0100

    csq

--- app/pages/players/[slug]/page.tsx
+++ app/pages/players/[slug]/page.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+'use client'
+import { usePathname } from 'next/navigation';
 
-export default function page() {
-  return <div>Le slug</div>;
-}
+const TeamPage = () => {
+  const pathname = usePathname();
+  const PlayerName = extractTeamNameFromPathname(pathname);
+
+  return (
+    <div>
+      <h1>{PlayerName}</h1>
+    </div>
+  );
+};
+
+export default TeamPage;
+
+const extractTeamNameFromPathname = (pathname: string): string => {
+  const parts = pathname.split('/');
+  return parts[parts.length - 1];
+};
