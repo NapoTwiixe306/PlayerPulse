eac6e968d6016d82bc3a8bd0a34c0bb24213804a
julien <julien.milants@gmail.com>
2024-01-05 11:32:19 +0100

    custom slug page

--- app/pages/team/[slug]/page.tsx
+++ app/pages/team/[slug]/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+'use client'
+import { usePathname } from 'next/navigation';
 
-export default function page() {
+const TeamPage = () => {
+  const pathname = usePathname();
+  const teamName = extractTeamNameFromPathname(pathname);
+
   return (
-    <div>ta mère</div>
-  )
-}
+    <div>
+      <h1>{teamName}</h1>
+    </div>
+  );
+};
+
+export default TeamPage;
+
+const extractTeamNameFromPathname = (pathname: string): string => {
+  const parts = pathname.split('/');
+  return parts[parts.length - 1];
+};
